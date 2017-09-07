63bd0f01fc7605066808914cfbf5519a0c210a91
stuzog <stuart@stuzog.com>
2017-09-07 11:48:03 -0700

    added more URL links

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^links/$', views.links, name='links'),
 ]
 
--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
-	
-	
+    return HttpResponse("Rango says: \"Hey there partner!\"")
+
+def about(request):
+    return HttpResponse("Rango says: 'Here is the About page'")
+
+def contact(request):
+    return HttpResponse("Here's how to contact us:")
+
+def links(request):
+    return HttpResponse("Some useful links:")
+
