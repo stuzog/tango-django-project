0c0c687fe9d7ec9adec98852bfa31652d8cc1d23
stuzog <stuart@stuzog.com>
2017-09-14 17:49:31 -0700

    changed rango.views.py for new templates

--- rango/views.py
+++ rango/views.py
@@ -23,18 +23,31 @@ def rangoIndex(request):
 
 
 def rangoAbout(request):
-    return HttpResponse("Rango says: \"Here is the About page\"")
+
+   # return HttpResponse("Rango says: \"Here is the About page\"")
+    context_dict = {'boldmessage': "About Rango we know very little."}
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def rangoLinks(request):
-    return HttpResponse("Some useful links:")
+#    return HttpResponse("Some useful links:")
+    context_dict = {'boldmessage': "Here are some useful links"}
+
+    return render(request, 'rango/links.html', context=context_dict)
 
 
 def rangoFiles(request):
-    return HttpResponse("Files you can download and use")
+#    return HttpResponse("Files you can download and use")
+    context_dict = {'boldmessage': "Here are some useful files"}
+
+    return render(request, 'rango/files.html', context=context_dict)
 
 
 def rangoContact(request):
-    return HttpResponse("Here's how to contact us:")
+#    return HttpResponse("Here's how to contact us:")
+    context_dict = {'boldmessage': "Here's how to contact us"}
+
+    return render(request, 'rango/contact.html', context=context_dict)
 
 
