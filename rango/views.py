62e927666cbbe2706166e4fd34013143f600a418
stuzog <stuart@stuzog.com>
2017-11-10 09:05:23 -0800

    show Categories on index page

--- rango/views.py
+++ rango/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
-
-# from django.http import HttpResponse
+from rango.models import Category
 
 
 def rangoindex(request):
-    # Construct a dictionary to pass to the template engine as its context
-    # Note that the key boldmessage is the same as {{ boldmessage }}
-    # in the template!
-    context_dict = {'boldmessage': "Get some crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by number of likes in descending order.
+    # Retrieve the top five only, or all if less than five.
+    # Replace the list on our context_dict dictionary
+    # that will be passed to the template engine.
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
+    # Render the response and send it back!
+    return render(request, 'rango/index.html', context_dict)
+
+
+# def rangoindex(request):
+#     # Construct a dictionary to pass to the template engine as its context
+#     # Note that the key boldmessage is the same as {{ boldmessage }}
+#     # in the template!
+#     context_dict = {'boldmessage': "Get some crunchy, creamy, cookie, candy, cupcake!"}
+#
+#     # Return a rendered response to send to the client.
+#     # We make use of the shortcut function to make our lives easier.
+#     # Note that the first parameter is the template we wish to use.
+#     return render(request, 'rango/index.html', context=context_dict)
+#
 
 def rangoabout(request):
     # return HttpResponse("Rango says: \"Here is the About page\"")
